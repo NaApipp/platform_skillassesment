4d67c01ddae3738f156813162913d3f869ae1a39
NaApipp <nabilapipp@gmail.com>
2026-07-18 19:11:02 +0700

    fix: fixing update route 'hasil-saya'

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -62,7 +62,7 @@ export default function Sidebar() {
                 fill="none"
                 />
               <a
-                href="hasil-saya"
+                href="/hasil-saya"
                 className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-200"
               >
                 {" "}
@@ -80,6 +80,7 @@ export default function Sidebar() {
             <UserStar
               className="size-5 text-gray-700 dark:text-gray-200"
               strokeWidth={2}
+              
               stroke="currentColor"
               fill="none"
             />
