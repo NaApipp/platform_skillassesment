8e4753343847cec4c58e05061669d3076296b06e
NaApipp <nabilapipp@gmail.com>
2026-07-22 20:51:27 +0700

    style: add style to dashboard page

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,10 +1,14 @@
 export default function Page() {
-    return (
-        <div className="min-h-screen">
-            <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">Selamat Datang di Platform SkillAssesment</h1>
-            <p className="text-gray-600 dark:text-gray-400">Silahkan pilih menu yang tersedia pada Sidebar untuk memulai penilaian skill</p>
-
-            
-        </div>
-    )
+  return (
+    <div className="min-h-screen m-14">
+      <h1 className="text-3xl font-bold text-gray-800 ">
+        Selamat Datang di Platform SkillAssesment
+      </h1>
+      <div className="w-20 mt-3 mb-5 border-2 border-solid border-amber-500" />
+      <p className="text-xl font-light dark:text-gray-400">
+        Silahkan pilih menu yang tersedia pada Sidebar untuk memulai penilaian
+        skill
+      </p>
+    </div>
+  );
 }
